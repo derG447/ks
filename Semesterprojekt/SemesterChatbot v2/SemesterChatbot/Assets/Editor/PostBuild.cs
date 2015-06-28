3c2ed6a34caaf8c14aa97afc5e2304d9705ace5d
Oliver <obeeck@cityweb.de>
2015-06-28 14:10:36 +0200

    Postbuild Optionen
    Kopieren von /aiml und /config nach Buildvorgang

---
+++ Semesterprojekt/SemesterChatbot v2/SemesterChatbot/Assets/Editor/PostBuild.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class PostBuild : MonoBehaviour {
+
+    [MenuItem("MyTools/Windows-Build with Postprocess")]
+    public static void BuildGame()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        //FileUtil.DeleteFileOrDirectory(path + "/BuiltGame.exe");
+
+        string[] levels = new string[] { "Assets/Scene/Main.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        FileUtil.ReplaceFile("Assets/Plugins/aiml", path + "/aiml");
+        FileUtil.ReplaceFile("Assets/Plugins/config", path + "/config");
+    }
+}
---
+++ Semesterprojekt/SemesterChatbot v2/SemesterChatbot/Assets/Editor/PostBuildRun.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Diagnostics;
+
+public class PostBuildRun : MonoBehaviour {
+
+    //[MenuItem("MyTools/Windows-Build with Postprocess and Run")]
+    public static void BuildGameAndRun()
+    {
+        // Get filename.
+        string path = EditorUtility.SaveFolderPanel("Choose Location of Built Game", "", "");
+        string[] levels = new string[] { "Assets/Scene/Main.unity" };
+
+        // Build player.
+        BuildPipeline.BuildPlayer(levels, path + "/BuiltGame.exe", BuildTarget.StandaloneWindows, BuildOptions.None);
+
+        // Copy a file from the project folder to the build folder, alongside the built game.
+        FileUtil.ReplaceFile("Assets/Plugins/aiml", path + "/aiml");
+        FileUtil.ReplaceFile("Assets/Plugins/config", path + "/config");
+
+        // Run the game (Process class from System.Diagnostics).
+
+        Process proc = new Process();
+        proc.StartInfo.FileName = path + "/BuiltGame.exe";
+        proc.Start();
+    }
+}
